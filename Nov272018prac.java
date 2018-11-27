6d43c248a0d1be7d106041019728179277f22e26
MithilD12 <45392148+MithilD12@users.noreply.github.com>
2018-11-27 08:44:20 -0500

    Created combine method
    Made a method that takes two seperate array lists and combines it into one array list

--- Nov272018prac.java
+++ Nov272018prac.java
@@ -2,5 +2,14 @@ import java.util.ArrayList;
 
 public class Nov272018prac{
 
-}
 
+
+public static ArrayList<Integer> combine(ArrayList<Integer> l1, ArrayList<Integer> l2){
+	ArrayList<Integer> list = new ArrayList<>();
+	for(int i = 0;i < l1.size();i++)
+		list.add(l1.get(i));
+	for(int i = 0;i < l1.size();i++)
+		list.add(l2.get(i));
+	return list;
+	}
+}
